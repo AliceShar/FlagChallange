43ad69b101868116df24119bda7d7be40408f085
Alisa Sharova <aliceshar777@gmail.com>
2025-01-23 12:51:41 +0200

    feat: project init

---
+++ FlagChallenge/FlagChallenge/FlagChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagChallengeApp.swift
+//  FlagChallenge
+//
+//  Created by Alice Sharova on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlagChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
